f25bb10873e749de74d9d6728aa916fb5e63a8e9
Vladimir <vzhuchkov@tut.by>
2022-09-13 23:05:37 +0300

    Done

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,12 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
-        int sum = 0;
-        int first= 1;
-        System.out.println(sum);
+        int first = 0;
+        int second = 1;
+        int sum;
         System.out.println(first);
+        System.out.println(second);
         for (int i = 2; i < lastFibonacci; i++) {
-            sum = sum(i-1) + (i-2);
+          sum = first+second;
+          first = second;
+          second = sum;
             System.out.println(sum);
         }
     }
